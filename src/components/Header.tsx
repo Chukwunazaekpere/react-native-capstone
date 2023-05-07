fca0dfc3a795aed743b1fb0c4d705f3da4ca69fc
Chukwunazaekpere <ceo.naza.tech@gmail.com>
2023-05-07 16:04:18 +0100

    auth setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { View, Image } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 const HeaderLogo = require("../assets/images/Logo.png");
 
 
@@ -11,5 +11,7 @@ const Header = () => {
     )
 };
 
-
+const style = StyleSheet.create({
+    
+})
 export default Header;
